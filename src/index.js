156884ad6a9af99eb5eabc43624c0a0b2ebda8b8
Dominic Mandy <dominicemandy@gmail.com>
2020-07-12 22:44:01 -0700

    editor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
+import monaco from 'monaco-editor';
 
 function component() {
 	const element = document.createElement('div');
 
+	monaco.editor.create(document.getElementById("container"), {
+		value: "function hello() {\n\talert('Hello world!');\n}",
+		language: "javascript"
+	});
 	// Lodash, currently included via a script, is required for this line to work
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
